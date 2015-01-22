bbc6b34be006176ef36a95b23446484232c66804
Bharat Sesham <FZGL40701BFe>
2015-01-22 19:48:39 +0530

    First_Commit

---
+++ src/com/project/handyproject/SwipeScreenExample.java
@@ -0,0 +1,63 @@
+package com.project.handyproject;
+
+//import com.android.swipe.R;
+import com.project.handyproject.SimpleGestureFilter.SimpleGestureListener;
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.content.res.Resources;
+import android.os.Bundle;
+import android.widget.Toast;
+import android.view.*;
+ 
+public class SwipeScreenExample extends Activity implements SimpleGestureListener{
+            private SimpleGestureFilter detector;
+            public String message;
+            public int num;
+            public static final String EXTRA_STRING_NAME = "extraStringName";
+            
+        @Override
+            public void onCreate(Bundle savedInstanceState) {
+                super.onCreate(savedInstanceState);
+                Intent intent = getIntent();
+                message = intent.getStringExtra(MainActivity.EXTRA_MESSAGE);
+                num = Integer.parseInt(message);
+                switch (num)
+                {
+                case 0 : setContentView(R.layout.swipe_screen_up);
+                case 1 : setContentView(R.layout.swipe_screen_down);
+                case 2 : setContentView(R.layout.swipe_screen_right);
+                case 3 : setContentView(R.layout.swipe_screen_left);
+                }
+                	
+                // Detect touched area 
+                detector = new SimpleGestureFilter(this,this);
+        }
+          
+    @Override
+    public boolean dispatchTouchEvent(MotionEvent me){
+        // Call onTouchEvent of SimpleGestureFilter class
+         this.detector.onTouchEvent(me);
+       return super.dispatchTouchEvent(me);
+    }
+    @Override
+     public void onSwipe(int direction, float slope) {
+      int x = direction + num*7;
+      String s = x + "";      
+      Toast.makeText(this, s, Toast.LENGTH_SHORT).show();
+      
+      Intent iData = new Intent();
+      iData.putExtra( EXTRA_STRING_NAME, s );
+      
+      setResult( android.app.Activity.RESULT_OK,iData );
+      
+      finish();
+     
+    }
+      
+     @Override
+     public void onDoubleTap() {
+        Toast.makeText(this, "Double Tap", Toast.LENGTH_SHORT).show();
+     }
+          
+  }
